5fe3bb58e44b9d368155aabc6abb56468df9f2cf
daraholic <1112dara@gmail.com>
2022-03-25 21:39:44 +0800

    convert value into json

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php date_default_timezone_set("Asia/Taipei");
 function exchange($from,$to){
     if($from==$to){
-        echo $rate=1; //幣別相同匯率=1
-        echo "<br>";
-        echo date("Y-m-d H:i:s");
+        $rate=1; //幣別相同匯率=1
+        // echo "<br>";
+        // echo date("Y-m-d H:i:s");
+        $result=array('exchange_rate'=>$rate,'updated_at'=>date("Y-m-d H:i:s"));//取現在時刻
+        // echo json_encode($result);
     }else{
         $content=file_get_contents('https://tw.rter.info/capi.php');
         $currency=json_decode($content);
@@ -16,15 +18,20 @@ function exchange($from,$to){
                 $date=new DateTime("$val->UTC");
             }
         }
+
         $rate=$to_val/$from_val;
-        echo round($rate,2); //小數點第二位四捨五入
-        echo "<br>";
-        $date->modify("+8 hours");
-        echo $date->format("Y-m-d H:i:s");
         
+        // echo round($rate,2); 
+        // echo "<br>";
+        // $date->modify("+8 hours");
+        // echo $date->format("Y-m-d H:i:s");
+
+        $result=array('exchange_rate'=>round($rate,2),'updated_at'=>$date->format("Y-m-d H:i:s"),);
     }
+    return json_encode($result);
 
 }
 echo exchange('JPY','TWD');
+
 ?>
 
